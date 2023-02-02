83bcc0472384eaca979babce19aafceb74622852
subin606 <gaemilee606@gmail.com>
2023-02-02 22:11:57 +0900

    initial commit

---
+++ src/main/java/teamfresh/api/TeamFreshAssignmentApplication.java
@@ -0,0 +1,13 @@
+package teamfresh.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFreshAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamFreshAssignmentApplication.class, args);
+	}
+
+}

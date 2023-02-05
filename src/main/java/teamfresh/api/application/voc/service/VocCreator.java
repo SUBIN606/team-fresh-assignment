3c33d81667f723f399f3016459375ec555d39561
subin606 <gaemilee606@gmail.com>
2023-02-05 19:12:19 +0900

    VOC 생성 서비스를 변경하라
    VOC 엔티티 변경사항에 따라 VOC 생성 서비스에서 추가적으로 정보를 받습니다.
    VOC 생성 서비스에서 필요로하는 입력 데이터를 서비스 내부에 `Command`라는 클래스를 생성해 관리합니다.

--- src/main/java/teamfresh/api/application/voc/service/VocCreator.java
+++ src/main/java/teamfresh/api/application/voc/service/VocCreator.java
@@ -1,5 +1,9 @@
 package teamfresh.api.application.voc.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import teamfresh.api.application.voc.blame.service.BlameCreator;
@@ -8,36 +12,41 @@ import teamfresh.api.application.voc.domain.Voc;
 import teamfresh.api.application.voc.domain.VocRepository;
 
 /** VOC 등록 담당 서비스 */
+@RequiredArgsConstructor
 @Service
 public class VocCreator {
 
     private final BlameCreator blameCreator;
     private final VocRepository repository;
 
-    public VocCreator(final BlameCreator blameCreator, final VocRepository repository) {
-        this.blameCreator = blameCreator;
-        this.repository = repository;
-    }
-
     /**
      * 주어진 데이터를 이용해 VOC를 등록합니다.
      *
-     * @param createdBy VOC 등록 담당자 id
-     * @param content 클레임 내용
-     * @param target 귀책 대상
-     * @param cause 귀책 사유
+     * @param command VOC 생성 요청 커맨드
+     * @return 생성된 VOC
      */
     @Transactional
-    public Voc create(Long createdBy,
-                       String content,
-                       BlameTarget target,
-                       String cause) {
+    public Voc create(Command command) {
         return repository.save(
                 Voc.withBlame(
-                        content,
-                        createdBy,
-                        blameCreator.create(target, cause)
+                        command.content,
+                        blameCreator.create(command.target, command.cause),
+                        command.customerManagerId,
+                        command.createdBy
                 )
         );
+    }
+
+    /**
+     * VOC 생성 요청 커맨드 객체입니다. VOC 생성에 필요한 모든 값을 포함합니다.
+     */
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Command {
+        private String content;
+        private BlameTarget target;
+        private String cause;
+        private Long customerManagerId;
+        private Long createdBy;
     }
 }

69ff4ea4360e0e95159c2e14963ad58f25009f06
subin606 <gaemilee606@gmail.com>
2023-02-08 18:25:23 +0900

    배상정보와 페널티를 양방향 연관관계로 매핑하라

--- src/main/java/teamfresh/api/application/compensation/domain/Compensation.java
+++ src/main/java/teamfresh/api/application/compensation/domain/Compensation.java
@@ -1,10 +1,13 @@
 package teamfresh.api.application.compensation.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
+import teamfresh.api.application.penalty.domain.Penalty;
 
 /** 배상정보 엔티티 */
 @Getter
@@ -17,19 +20,23 @@ public class Compensation {
 
     private int amount;
 
+    @OneToOne(mappedBy = "compensation", fetch = FetchType.LAZY)
+    private Penalty penalty;
+
     protected Compensation() {
     }
 
-    private Compensation(final Long id, final int amount) {
+    public Compensation(final Long id, final int amount, final Penalty penalty) {
         this.id = id;
         this.amount = amount;
+        this.penalty = penalty;
     }
 
     public static Compensation of(Long id, int amount) {
-        return new Compensation(id, amount);
+        return new Compensation(id, amount, null);
     }
 
     public static Compensation of(int amount) {
-        return new Compensation(null, amount);
+        return new Compensation(null, amount, null);
     }
 }
